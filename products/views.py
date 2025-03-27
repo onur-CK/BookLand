1ad97c7d77e7c41e5f97537c50d587ac68a37d5f
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-03-28 00:40:07 +0100

    Move newsletter section above footer.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def all_products(request):
+    """A view to show all products"""
     return render(request, 'products/products.html')
 
 def product_detail(request):
+    """A veiw to show individual product deatails"""
     return render(request, 'products/product_details.html')
