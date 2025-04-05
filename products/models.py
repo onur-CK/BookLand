306f5c8733252cbe05e4c316bf770288164fc858
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-05 09:03:03 +0200

    Create Book model class.

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -12,5 +13,22 @@ class Category(models.Model):
     
     def get_friendly_name(self):
         return self.friendly_name or self.name
+    
+class Book(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True,
+                                 validators=[MinValueValidator(0), MaxValueValidator(5)])
+    image = models.ImageField(null=True, blank=True)
+    inventory = models.IntegerField(default=0)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
     
 
