f97668937ddeadf9f95a70ebfc806af8a32ded25
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-05 09:44:28 +0200

    Add category filter and search query filter to all_products view.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from .models import Book, Category
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import reverse, redirect
 
 def all_products(request):
     """A view to show all products"""
-    return render(request, 'products/products.html')
+    
+    products = Book.objects.filter(available=True)
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        # Handle categories filter
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # Handle search queries
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            queries = Q(title__icontains=query) | Q(description__icontains=query) | Q(author__icontains=query)
+            products = products.filter(queries)
+
 
 def product_detail(request):
     """A veiw to show individual product deatails"""
