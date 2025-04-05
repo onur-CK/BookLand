3f9ddbbbaef6e9927250af94a0a2c667e0b3e742
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-05 09:17:24 +0200

    Create Category and Book admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Book
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+    search_fields = ('name', 'friendly_name')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'price', 'rating', 'available', 'inventory')
+    list_filter = ('category', 'available')
+    search_fields = ('title', 'author', 'description')
+    ordering = ('title',)
+    readonly_fields = ('created_at', 'updated_at')
+    list_editable = ('price', 'available', 'inventory')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
