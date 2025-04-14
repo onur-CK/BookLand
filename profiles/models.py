16e14abe38e77d50424a0098e0a8b3266a774072
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-14 16:00:56 +0200

    Create user profile model.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_street_address = models.CharField(max_length=80, null=True, blank=True)
+    default_apartment = models.CharField(max_length=80, null=True, blank=True)
+    default_city = models.CharField(max_length=40, null=True, blank=True)
+    default_postal_code = models.CharField(max_length=20, null=True, blank=True)
+    default_country = models.Charfield(max_length=40, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    
