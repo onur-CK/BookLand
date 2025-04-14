4130de0877c6ab577c158774e6f84af37802a6ca
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-14 16:08:28 +0200

    Create create or update user profile function.

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserProfile(models.Model):
     """
@@ -14,4 +17,16 @@ class UserProfile(models.Model):
     default_country = models.Charfield(max_length=40, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return self.user.username
     
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    #Existing users: just save the profile
+    instance.userprofile.save()
+
