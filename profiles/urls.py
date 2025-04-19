53eb3d2e333a87c24c8c4ad39d359a52ceb4c170
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-19 20:00:33 +0200

    Create add and remove wishlist urls.

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/', views.order_history, name='order_history'),
     path('wishlist/', views.wishlist, name='wishlist'),
+    path('add_to_wishlist/<int:book_id>', views.add_to_wishlist, name='add_to_wishlist'),
+    path('remove_from_wishlist/<int:book_id>', views.remove_from_wishlist, name='remove_from_wishlist')
 ]
