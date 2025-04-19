cb914dbe6e9d35462d0f2f092ebc8e472c32731c
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-19 20:34:58 +0200

    Add UserProfileAdmin and WishListAdmin into admin.py in profiles app.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import UserProfile, WishlistItem
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_phone_number', 'default_city', 'default_country')
+    search_fields = ('user__username', 'user__email', 'default_phone_number')
+    list_filter = ('default_country',)
+
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'date_added')
+    search_fields = ('user__username', 'book__title')
+    list_filter = ('date_added',)
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(WishlistItem, WishlistItemAdmin)
