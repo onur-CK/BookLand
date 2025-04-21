c221306426012ea9e661afe9ce4b1dbf076d9f7a
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-21 20:26:13 +0200

    Create checkout view and form data for post request.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
+def checkout(request):
+    """
+    Handle the checkout process
+    """
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart at the moment")
+        return redirect(reverse('products'))
+
+    if request.method == 'POST':
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address': request.POST['street_address'],
+            'apartment': request.POST['apartment'],
+            'city': request.POST['city'],
+            'postal_code': request.POST['postal_code'],
+            'country': request.POST['country'],
+        }
+       
