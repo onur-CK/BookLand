67a7f62353ddb01e88fbea4dd84bea56c2fea710
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-21 19:50:45 +0200

    Add the fields for order form.

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,4 +25,12 @@ class OrderForm(forms.ModelForm):
             'country': 'Country',
         }
 
-        
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'form-control'
+            self.fields[field].label = False
