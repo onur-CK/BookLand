58fa053f40814da9f9fe7cb99f6e6f70c6bc704b
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-21 17:49:11 +0200

    Add save function to order class.

--- checkout/models.py
+++ checkout/models.py
@@ -35,5 +35,19 @@ class Order(models.Model):
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.grand_total = self.order_total + self.shipping_cost
         self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+    
+
     
 
