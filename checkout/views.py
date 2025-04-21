1427f27881b315431aab37a27e5ab16e21ff6591
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-21 20:48:58 +0200

    Add order form validation and create the dictionary with context.

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,12 @@
+# checkout/views.py
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.conf import settings
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Book
+from profiles.models import UserProfile
+from cart.contexts import cart_contents
 
 def checkout(request):
     """
@@ -21,4 +28,39 @@ def checkout(request):
             'postal_code': request.POST['postal_code'],
             'country': request.POST['country'],
         }
-       
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, quantity in cart.items():
+                try:
+                    book = Book.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        book=book,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
+                except Book.DoesNotExist:
+                    messages.error(request, "One of the books in your cart wasn't found in our database.")
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            # Save the info to the user's profile if all is well
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. Please double check your information.')
+    else:
+        order_form = OrderForm()
+
+    current_cart = cart_contents(request)
+    context = {
+        'order_form': order_form,
+        'cart_items': current_cart['cart_items'],
+        'total': current_cart['total'],
+        'product_count': current_cart['product_count'],
+        'shipping': current_cart['shipping'],
+        'grand_total': current_cart['grand_total'],
+    }
+
+    return render(request, 'checkout/checkout.html', context)
