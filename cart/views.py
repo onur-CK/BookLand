1e515007f8047dd5b72eb5f47f5762c2d0aa4c2f
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-21 16:59:13 +0200

    Create view_cart and  add to cart views.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from products.models import Book
 
-# Create your views here.
+def view_cart(request):
+    """A view to return the shopping cart page"""
+    return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """Add a quantity of the specified product to the shopping cart"""
+    book = get_object_or_404(Book, pk=item_id)
+    quantity = int(request.POST.get('quantity', 1))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+        messages.success(request, f'Updated {book.title} quantity to {cart[item_id]}')
+    else:
+        cart[item_id] = quantity
+        messages.success(request, f'Added {book.title} to your cart')
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
+
+
