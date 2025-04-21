b904318fa8bccbc833481625bdfdc624304e7402
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-21 17:34:09 +0200

    Create class order model.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from products.models import Book
+from profiles.models import UserProfile
 
-# Create your models here.
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, 
+                                    null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address = models.CharField(max_length=80, null=False, blank=False)
+    apartment = models.CharField(max_length=80, null=True, blank=True)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    postal_code = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    shipping_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=5)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
