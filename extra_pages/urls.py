d5e1335053c089faedc53cc2867ee83aae81e622
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-22 22:53:53 +0200

    Create privacy and policy page and related urls.

--- extra_pages/urls.py
+++ extra_pages/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.contact_us, name='contact_us'),
     path('faq/', views.faq, name='faq'),
     path('shipping_policy/', views.shipping_policy, name='shipping_policy'),
+    path('returns/', views.returns, name='returns'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
 ]
--- extra_pages/views.py
+++ extra_pages/views.py
@@ -11,3 +11,11 @@ def faq(request):
 def shipping_policy(request):
     """Render the Shipping Policy page"""
     return render(request, 'extra_pages/shipping_policy.html')
+
+def returns(request):
+    """Render the Returns and Exchanges page"""
+    return render(request, 'extra_pages/returns.html')
+
+def privacy_policy(request):
+    """Render the Privacy Policy page"""
+    return render(request, 'extra_pages/privacy_policy.html')
