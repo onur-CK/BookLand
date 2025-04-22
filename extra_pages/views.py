29943e0cd241328d61f4dbba64ff1716461f1aac
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-22 23:30:26 +0200

    Change the way we display the success message after submitting the contact us form by sperating the cart success message from our contact us page message.

--- extra_pages/views.py
+++ extra_pages/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def contact_us(request):
-    """Render the Contact Us page"""
-    return render(request, 'extra_pages/contact_us.html')
+    """Render the Contact Us page and handle form submission"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        
+        # Set a session variable for the success message
+        request.session['contact_form_submitted'] = True
+        request.session['contact_name'] = name
+        
+        # Redirect back to the contact page to prevent form resubmission on refresh
+        return redirect('contact_us')
+    
+    # Check if we need to display a success message
+    contact_success = False
+    contact_name = ''
+    if request.session.get('contact_form_submitted'):
+        contact_success = True
+        contact_name = request.session.get('contact_name', '')
+        # Clear the session variables
+        request.session.pop('contact_form_submitted', None)
+        request.session.pop('contact_name', None)
+    
+    return render(request, 'extra_pages/contact_us.html', {
+        'contact_success': contact_success,
+        'contact_name': contact_name,
+    })
 
 def faq(request):
     """Render the Frequently Asked Questions page"""
