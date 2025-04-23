3d8369f7724a0ef4a6a93c077154b8f3783a2f1c
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-23 17:05:29 +0200

    Add comments and source link for checkout/signals.

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,3 +1,4 @@
+# Source: https://docs.djangoproject.com/en/5.1/topics/signals/
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
@@ -6,13 +7,22 @@ from .models import OrderLineItem
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on lineitem update/create
+    Update order total when OrderLineItem is created or updated
+    
+    Parameters:
+    - sender: The model class (OrderLineItem)
+    - instance: The actual instance being saved
+    - created: Boolean flag for new instances vs updates
+    - **kwargs: Additional arguments
     """
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
-    Update order total on lineitem delete
+    Update order total when OrderLineItem is deleted
+    
+    This ensures the order total is recalculated whenever
+    a line item is removed from an order.
     """
     instance.order.update_total()
