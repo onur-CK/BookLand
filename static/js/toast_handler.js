ca19806c4451658b746f08200d462774ce3a06ee
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-23 12:26:17 +0200

    Add comments and source link for toast handler

--- static/js/toast_handler.js
+++ static/js/toast_handler.js
@@ -1,5 +1,8 @@
+// Toast notification initialization script 
+// Source: https://getbootstrap.com/docs/5.3/components/toasts/#javascript
+
 document.addEventListener("DOMContentLoaded", function () {
-
+    // Show all toasts when the page loads
+    // Uses jQuery selector to find all toast elements and trigger their show method
     $('.toast').toast('show');
-
 });
