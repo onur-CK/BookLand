251d985e6eed4232c88ea1ff911278c0af43372d
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-29 17:26:16 +0200

    Create delete testimonial page and admin panel for approval.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, WishlistItem
+from .models import UserProfile, WishlistItem, Testimonial
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'default_phone_number', 'default_city', 'default_country')
@@ -13,3 +13,23 @@ class WishlistItemAdmin(admin.ModelAdmin):
 
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(WishlistItem, WishlistItemAdmin)
+
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'rating', 'date_created', 'is_approved')
+    list_filter = ('is_approved', 'rating', 'date_created')
+    search_fields = ('user__username', 'title', 'content')
+    list_editable = ('is_approved',)
+    readonly_fields = ('date_created', 'date_updated')
+    actions = ['approve_testimonials', 'unapprove_testimonials']
+    
+    def approve_testimonials(self, request, queryset):
+        queryset.update(is_approved=True)
+        self.message_user(request, f"{queryset.count()} testimonials approved.")
+    approve_testimonials.short_description = "Approve selected testimonials"
+    
+    def unapprove_testimonials(self, request, queryset):
+        queryset.update(is_approved=False)
+        self.message_user(request, f"{queryset.count()} testimonials unapproved.")
+    unapprove_testimonials.short_description = "Unapprove selected testimonials"
+
+admin.site.register(Testimonial, TestimonialAdmin)
