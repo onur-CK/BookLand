e572484487eb8ffbf48d443f81e0f6b7bab8194e
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-04-29 14:51:47 +0200

    Create testimonial page.

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('wishlist/', views.wishlist, name='wishlist'),
     path('add_to_wishlist/<int:book_id>', views.add_to_wishlist, name='add_to_wishlist'),
     path('remove_from_wishlist/<int:book_id>', views.remove_from_wishlist, name='remove_from_wishlist'),
+    # Testimonial URLs
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('testimonials/edit/<int:testimonial_id>/', views.edit_testimonial, name='edit_testimonial'),
+    path('testimonials/delete/<int:testimonial_id>/', views.delete_testimonial, name='delete_testimonial'),
 ]
