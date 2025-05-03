173d8508b272b60aca08f6c99305ef64750b687b
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-05-03 16:01:01 +0200

    Movee products related js into a seperate js file inside related app's static file.

---
+++ products/static/products/js/products.js
@@ -0,0 +1,21 @@
+// Add event listener to sort dropdown to reload page with sort parameters
+document.getElementById('sort-selector').addEventListener('change', function() {
+    var selector = this;
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.value;
+    if(selectedVal != "reset"){
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+        
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl);
+    }
+});
