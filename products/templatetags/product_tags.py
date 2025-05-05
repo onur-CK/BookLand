7e77b12f995fc1f0df405b1b3fd73a49add1cfbd
onur-CK <118271710+onur-CK@users.noreply.github.com>
2025-05-05 02:10:11 +0200

    Delete the save method to dsplay the orginal image.

--- products/templatetags/product_tags.py
+++ products/templatetags/product_tags.py
@@ -4,12 +4,8 @@ register = template.Library()
 
 @register.filter
 def webp_image_url(image_url):
-    """Convert image URL to WebP format"""
-    if not image_url:
-        return None
-    
-    # Split the URL and change extension to webp
-    path_parts = image_url.rsplit('.', 1)
-    webp_url = f"{path_parts[0]}.webp"
-    
-    return webp_url
+    """
+    Currently returns the original image URL.
+    For WebP conversion, a more complex solution with S3 would be needed.
+    """
+    return image_url
